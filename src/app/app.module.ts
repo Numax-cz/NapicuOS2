dfae2433ad5c0c77f9ad4c1229af6260a2a5f132
Marcel <numax.cz@protonmail.ch>
2022-12-22 20:39:28 +0100

    Přidány základní componety

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SplashScreenComponent } from './bios/components/splash-screen/splash-screen.component';
-import { ConfigurationComponent } from './bios/components/configuration/configuration.component';
-import { TextScreenComponent } from './bios/components/text-screen/text-screen.component';
 import {BiosModule} from "./bios/bios.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SplashScreenComponent,
-    ConfigurationComponent,
-    TextScreenComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/bios/bios.module.ts
+++ src/app/bios/bios.module.ts
@@ -5,6 +5,12 @@ import {ConfigurationComponent} from "./components/configuration/configuration.c
 import {TextScreenComponent} from "./components/text-screen/text-screen.component";
 import {NgModule} from "@angular/core";
 import {BiosTextScreenGuard} from "./guards/text-screen.guard";
+import {SplashScreenComponent} from "./components/splash-screen/splash-screen.component";
+import { MainComponent } from './components/configuration/components/main/main.component';
+import { AdvancedComponent } from './components/configuration/components/advanced/advanced.component';
+import { BootComponent } from './components/configuration/components/boot/boot.component';
+import { ToolsComponent } from './components/configuration/components/tools/tools.component';
+import { ExitComponent } from './components/configuration/components/exit/exit.component';
 
 const routes: Routes = [
   {path: NapicuConfig.Path.BIOS_CONFIGURATION_ROOT_PATH, component: ConfigurationComponent},
@@ -13,6 +19,16 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [
+    SplashScreenComponent,
+    ConfigurationComponent,
+    TextScreenComponent,
+    MainComponent,
+    AdvancedComponent,
+    BootComponent,
+    ToolsComponent,
+    ExitComponent,
+  ]
 })
 export class BiosModule { }
