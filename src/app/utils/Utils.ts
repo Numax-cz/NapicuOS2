89981a407efa4acb970a96ffb7ea3ea05b01da2b
Marcel <numax.cz@protonmail.ch>
2023-01-14 18:50:04 +0100

    Přidán script

--- src/app/utils/Utils.ts
+++ src/app/utils/Utils.ts
@@ -4,10 +4,13 @@ import {Console} from "./Console";
 import {Debug} from "./Debug";
 import {WebManager} from "./WebManager";
 
+declare type ValueOf<T> = T[keyof T];
+
 export {
   Cookies,
   AssetsCache,
   Console,
   Debug,
   WebManager,
+  ValueOf
 }
