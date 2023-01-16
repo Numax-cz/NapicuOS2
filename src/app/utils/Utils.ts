26527a51032c8256c0c1c5521f7241043eaef5b0
Marcel <numax.cz@protonmail.ch>
2023-01-16 15:56:44 +0100

    Přidána funkce CopyArray

--- src/app/utils/Utils.ts
+++ src/app/utils/Utils.ts
@@ -3,6 +3,7 @@ import {AssetsCache} from "./AssetsCache";
 import {Console} from "./Console";
 import {Debug} from "./Debug";
 import {WebManager} from "./WebManager";
+import {CopyArray} from "./CopyArray";
 
 declare type ValueOf<T> = T[keyof T];
 
@@ -12,5 +13,6 @@ export {
   Console,
   Debug,
   WebManager,
-  ValueOf
+  ValueOf,
+  CopyArray
 }
