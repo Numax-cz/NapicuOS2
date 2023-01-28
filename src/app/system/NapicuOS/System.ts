bcfc2c5f1b170133ae35f1740bda2bd6d13a9236
Marcel <numax.cz@protonmail.ch>
2023-01-28 19:04:02 +0100

    Přidána základní struktura

---
+++ src/app/system/NapicuOS/System.ts
@@ -0,0 +1,10 @@
+import * as NapicuKernel from "@Napicu/System/Kernel";
+
+
+export class NapicuOS extends NapicuKernel.Kernel{
+  protected readonly system_name: string = "NapicuOS";
+
+  protected main(): void {
+  }
+
+}
