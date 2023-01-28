ca59968ab64b89fcba341a58d901d43d3304ef5e
Marcel <numax.cz@protonmail.ch>
2023-01-28 10:16:48 +0100

    Upravena konfigurace, upraveny guards

---
+++ src/app/computer/guards/is-running-guard.service.ts
@@ -0,0 +1,27 @@
+import * as NapicuComputer from "@Napicu/VirtualComputer";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IsRunningGuard implements CanActivate {
+  constructor(private router: Router) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!NapicuComputer.VirtualComputer.get_is_running()) {
+      this.router.navigate(['']);
+      return false;
+    }
+    return true;
+  }
+}
