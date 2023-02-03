f56c1629a91d2ef489a88a6b86e80da3211583ff
Marcel <numax.cz@protonmail.ch>
2023-02-03 13:11:56 +0100

    Uprava flash componentu

--- src/app/bios/components/configuration/flash-screen/flash-screen.component.ts
+++ src/app/bios/components/configuration/flash-screen/flash-screen.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CopyArray} from "@Napicu/Utils/CopyArray";
 import {Bios} from "@Napicu/Bios/Bios";
+import {BiosConfig} from "@Napicu/Config/bios/Bios";
 
 @Component({
   selector: 'app-flash-screen',
@@ -8,6 +9,13 @@ import {Bios} from "@Napicu/Bios/Bios";
   styleUrls: ['./flash-screen.component.scss']
 })
 export class FlashScreenComponent implements OnInit, OnDestroy{
+
+  public is_active_drive_selection: boolean = true;
+
+  public selected_drive: number = 0;
+
+  public selected_dir: number = 0;
+
 
   public ngOnInit() {
     window.addEventListener("keydown", this.onKeyDownEvent);
@@ -19,5 +27,34 @@ export class FlashScreenComponent implements OnInit, OnDestroy{
 
   protected onKeyDownEvent = (e: KeyboardEvent): void => {
 
+  }
+
+
+  protected on_enter(): void {
+
+  }
+
+  protected on_tab(): void {
+    this.is_active_drive_selection = !this.is_active_drive_selection;
+  }
+
+  protected on_esc(): void {
+
+  }
+
+  protected move_up(): void {
+
+  }
+
+  protected move_down(): void {
+
+  }
+
+  public get_drives(): any{
+
+  }
+
+  public get_bios_version(): string{
+    return BiosConfig.BIOS_VERSION;
   }
 }
