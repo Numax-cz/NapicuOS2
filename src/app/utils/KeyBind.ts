7ba66b048af1faa9da4c1d7a3951767ffcafbb84
Marcel <numax.cz@protonmail.ch>
2023-02-15 17:10:02 +0100

    Upraveno keybindování

--- src/app/utils/KeyBind.ts
+++ src/app/utils/KeyBind.ts
@@ -1,6 +1,9 @@
-export function KeyBind(event: KeyboardEvent, key_code: number,  bind_function: (event: KeyboardEvent) => void, preventDefault: boolean = true): void {
-  if(event.keyCode === key_code){
-    bind_function(event);
-    if(preventDefault) event.preventDefault();
+export function KeyBind(event: KeyboardEvent, key_code: number | number[],  bind_function: (event: KeyboardEvent) => void, preventDefault: boolean = true): void {
+  const keys: number[] =  (typeof key_code === "object") ? key_code : [key_code];
+  for(const key of keys){
+    if(event.keyCode === key){
+      bind_function(event);
+      if(preventDefault) event.preventDefault();
+    }
   }
 }
