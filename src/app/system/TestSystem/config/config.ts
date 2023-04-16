10de737b96068f901033a23abb46ad16cdbbf924
Marcel <numax.cz@protonmail.ch>
2023-04-16 13:48:16 +0200

    Opraveny bugy

---
+++ src/app/system/TestSystem/config/config.ts
@@ -0,0 +1,7 @@
+import {TestSystemConfigInterface} from "../interface/config";
+import {SystemCookiesKernelDataInterface} from "@Napicu/System/Kernel/interface/Kernel";
+
+
+export const TestSystemDefaultConfig: SystemCookiesKernelDataInterface<TestSystemConfigInterface> = {
+  data: {}
+}
--- src/app/system/TestSystem/system.ts
+++ src/app/system/TestSystem/system.ts
@@ -4,11 +4,15 @@ import {ProcessManagerProcessTable} from "@Napicu/System/Kernel/interface/Proces
 import {SystemBaseProcessProgramsID} from "./programs/SysPrograms";
 import {TestSystemComponent} from "./components/system/system.component";
 import {CookiesConfigurator} from "@Napicu/System/Kernel/core/CookiesConfigurator";
+import {SystemCookiesKernelDataInterface} from "@Napicu/System/Kernel/interface/Kernel";
+import {TestSystemConfigInterface} from "./interface/config";
+import {TestSystemDefaultConfig} from "./config/config";
 
 export class TestSystem extends Kernel{
   protected readonly system_name: string = "system";
 
-  public declare system_config: CookiesConfigurator<string>;
+  public system_config: CookiesConfigurator<SystemCookiesKernelDataInterface<TestSystemConfigInterface>> =
+    new CookiesConfigurator<SystemCookiesKernelDataInterface<TestSystemConfigInterface>>(this.system_name, TestSystemDefaultConfig)
 
   public readonly initialized_system_process_table: ProcessManagerProcessTable[] = [
     { process: new TestProgram(), program_id: SystemBaseProcessProgramsID.TestProgram },
