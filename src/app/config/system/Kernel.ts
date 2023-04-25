e072a29af4e8df570cc416d60b0746059afc5dde
Marcel <numax.cz@protonmail.ch>
2023-04-25 19:26:17 +0200

    Přejmenována konfigurace

---
+++ src/app/config/system/Kernel.ts
@@ -0,0 +1,10 @@
+export namespace SystemConfig{
+  export const PRODUCTION_CONFIGURATION: boolean = false;
+
+  export const KERNEL_VERSION: string = "version 1.01-DEVELOPMENT";
+  export const KERNEL_VERSION_COMPANY_NAME: string = "NAPICU KERNEL";
+
+
+
+
+}
--- src/app/config/system/System.prod.ts
+++
@@ -1,9 +0,0 @@
-export namespace SystemConfig{
-  export const PRODUCTION_CONFIGURATION: boolean = false;
-
-
-
-
-
-
-}
